f6fea5ca3526600382b6353d50560f8664bed95c
Alex Fernandez <afernandez@unitec.edu>
2014-01-29 20:00:31 -0600

    Persons controller added

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mhotivo.Controllers
+{
+    public class PersonsController : Controller
+    {
+        //
+        // GET: /Persons/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
