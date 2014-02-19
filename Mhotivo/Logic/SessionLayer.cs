72433718e77d422d4b4f6ad33b7d0bedb14adb07
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2014-02-19 15:02:18 -0600

    Loading User Role on Session Layer Fixed

--- Mhotivo/Logic/SessionLayer.cs
+++ Mhotivo/Logic/SessionLayer.cs
@@ -76,18 +76,11 @@ namespace Mhotivo.Logic
         {
             using (var ctx = new MhotivoContext())
             {
-                var myUser = (from u in ctx.Users
-                              where u.Email.Equals(userName)
-                              select u);
-                if (!myUser.Any()) return null;
-                var singleUser = myUser.First();
-                Role role = null;
-                if (singleUser != null)
-                    role = singleUser.Role;
-                if (role != null)
-                    return role.Name;
+                User user = ctx.Users.Where(x => x.Email.Equals(userName)).Include(x => x.Role).FirstOrDefault();
+                if(user!=null)
+                    return user.Role.Name;
+                return "Guest";
             }
-            return "admin";
         }
 
 
