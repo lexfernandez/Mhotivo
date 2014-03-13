89b3a002cf1de0e2e61ceba2b3027bf35403aab5
solis1492 <solis1492@gmail.com>
2014-03-13 15:36:38 -0600

    Models for Spring 3 created. Also, the model for Meister was added to the project as those changes from the Meister Branch have not yet been merged.

---
+++ Mhotivo/Models/ClassActivityGradingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mhotivo.Models
+{
+    [Table("ClassActivityGrading")]
+    public class ClassActivityGrading
+    {
+        public virtual ClassActivity ClassActivity { get; set; }
+        public virtual Student Student { get; set; }
+        public double Score { get; set; }
+        public double Percentage { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ Mhotivo/Models/GradeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mhotivo.Models
+{
+    [Table("Grade")]
+    public class Grade
+    {
+        public string Name { get; set; }
+        public string EducationLevel { get; set; }
+    }
+}
