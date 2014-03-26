1b7a65bb94c56b4311f554cc96bc34e61dd5728e
Alex Fernandez <a.fernandez@mayanx.com>
2014-03-26 14:46:51 -0600

    Course CRUD Added

---
+++ Mhotivo/App_Data/Repositories/CourseRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using Mhotivo.Models;
+
+namespace Mhotivo.App_Data.Repositories
+{
+    public interface ICourseRepository :  IDisposable
+    {
+        Course First(Expression<Func<Course, Course>> query);
+        Course GetById(long id);
+        Course Create(Course itemToCreate);
+        IQueryable<TResult> Query<TResult>(Expression<Func<Course, TResult>> expression);
+        IQueryable<Course> Filter(Expression<Func<Course, bool>> expression);
+        Course Update(Course itemToUpdate);
+        void Delete(Course itemToDelete);
+        void SaveChanges();
+    }
+
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly MhotivoContext _context;
+
+        private CourseRepository(MhotivoContext ctx)
+        {
+            _context = ctx;
+           
+        }
+
+        public static CourseRepository Instance
+        {
+            get { return new CourseRepository(new MhotivoContext()); }
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public Course First(Expression<Func<Course, Course>> query)
+        {
+            var courses = _context.Courses.Select(query);
+            return courses.Count() != 0 ? courses.First() : null;
+        }
+
+        public Course GetById(long id)
+        {
+            var courses = _context.Courses.Where(x => x.CourseId == id);
+            return courses.Count() != 0 ? courses.First() : null;
+        }
+
+        public Course Create(Course itemToCreate)
+        {
+            var role = _context.Courses.Add(itemToCreate);
+            _context.SaveChanges();
+            return role;
+        }
+
+        public IQueryable<TResult> Query<TResult>(Expression<Func<Course, TResult>> expression)
+        {
+            return _context.Courses.Select(expression);
+
+        }
+
+        public IQueryable<Course> Filter(Expression<Func<Course, bool>> expression)
+        {
+            return _context.Courses.Where(expression);
+        }
+
+        public Course Update(Course itemToUpdate)
+        {
+            _context.Entry(itemToUpdate).State = EntityState.Modified;
+            SaveChanges();
+            return itemToUpdate;
+        }
+
+        public Course UpdateNew(Course itemToUpdate)
+        {
+            var role = GetById(itemToUpdate.CourseId);
+            role.Name = itemToUpdate.Name;
+            role.Area = itemToUpdate.Area;
+
+            return Update(role);
+        }
+
+        public void Delete(Course itemToDelete)
+        {
+            _context.Courses.Remove(itemToDelete);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ Mhotivo/Controllers/CourseController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mhotivo.App_Data.Repositories;
+
+namespace Mhotivo.Controllers
+{
+
+    public class CourseController : Controller
+    {
+
+        private readonly CourseRepository _roleRepo = CourseRepository.Instance;
+
+        //
+        // GET: /Course/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Course/Create
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Course/Create
+
+        [HttpPost]
+        public ActionResult Add(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Course/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Course/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // POST: /Course/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
