6772890d53bd324aed2cdd253bc97945d5fc59a3
Franklin Castellanos <frank_nightmare@unitec.edu>
2014-04-23 16:15:40 -0600

    Logic For creating rows in AcademicYear

--- Mhotivo/Logic/AcademicYearLogic.cs
+++ Mhotivo/Logic/AcademicYearLogic.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Mhotivo.App_Data.Repositories;
 using Mhotivo.Models;
 
@@ -11,17 +9,31 @@ namespace Mhotivo.Logic
     {
         private readonly ICourseRepository _courseRepo = CourseRepository.Instance;
         private readonly IPensumRepository _pensumRepo = PensumRepository.Instance;
+        private readonly IAcademicYearRepository _academicYearRepo = AcademicYearRepository.Instance;
 
         public void GenerateSectionForGrades(IEnumerable<Grade> grades )
         {
             var courses = _courseRepo.Query(x => x);
 
-            foreach (var c in courses)
+            foreach (var currentCourse in courses)
             {
-                var pensums = _pensumRepo.Filter(x => x.Course.CourseId == c.CourseId);
+                var pensums = _pensumRepo.Filter(x => x.Course.CourseId == currentCourse.CourseId);
                 foreach (var pensum in pensums)
                 {
-                    
+                    var academicYear = new AcademicYear
+                    {
+                        Approved = false,
+                        Course = currentCourse,
+                        Grade = pensum.Grade,
+                        IsActive = true,
+                        Room = "",
+                        Section = 'A',
+                        StudentsCount = 0,
+                        StudentsLimit = 25,
+                        Year = DateTime.Now
+                    };
+
+                    _academicYearRepo.Create(academicYear);
                 }
             }
         }
