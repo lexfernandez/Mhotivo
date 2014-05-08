025823cf8498bc6269fcfa33a6289d6a0fe417c2
Alex Fernandez <a.fernandez@mayanx.com>
2014-05-07 21:19:49 -0600

    Automatic Migrations Added

---
+++ Mhotivo/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Mhotivo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Mhotivo.App_Data.MhotivoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Mhotivo.App_Data.MhotivoContext";
+        }
+
+        protected override void Seed(Mhotivo.App_Data.MhotivoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
