b152df4f672e1d9d978e220d6b273f448c5ba9db
Alex Fernandez <a.fernandez@mayanx.com>
2014-05-08 02:49:34 -0600

    Ninject Added

---
+++ Mhotivo/App_Start/InjectModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mhotivo.App_Data.Repositories;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace Mhotivo.App_Start
+{
+    public class InjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRoleRepository>().To<RoleRepository>();
+        }
+    }
+}
